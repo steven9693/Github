1c5e8f46df9989100edf8bb87bd2ac126a9b8836
me <704296931@qq.com>
2018-05-15 19:15:53 +0800

    添加小说URL抓取
    音频链接抓取curlAction

--- Xiaoshuo/Home/Controller/IndexController.class.php
+++ Xiaoshuo/Home/Controller/IndexController.class.php
@@ -16,8 +16,20 @@ class IndexController extends Controller {
     public function index(){
 
         //采集某页面所有的超链接
-        $data = QueryList::Query('http://www.woaitingshu.com/video/16160-0-1.html', array('href' => array('script','html')),'','UTF-8','GB2312')->data;
+        $data = QueryList::Query('http://www.ting56.com/mp3/8256.html', array('href' => array('#vlink_1 a','href')),'','UTF-8','GB2312')->data;
         //打印结果
-        dump($data);
+
+        $result=array();
+
+        for($i=0;$i<2;$i++){
+            $result[]=QueryList::Query('http://www.ting56.com'.$data[$i]['href'], array('href' => array('script','html')),'','UTF-8','GB2312')->getData(function($item){
+                if($item['href']!=''){
+                    return $item['href'];
+                }
+            });
+        }
+
+        dump($result);
+
     }
 }
