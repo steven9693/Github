43c61da7b7c8632be48555ee273315e5436c91e7
steven9693 <704296931@qq.com>
2018-05-31 22:36:39 +0800

    修改URL

--- Xiaoshuo/Home/View/js/urls.js
+++ Xiaoshuo/Home/View/js/urls.js
@@ -1,5 +1,12 @@
+
 var DOMAIN="http://localhost/Github/Xiaoshuo/index.php?";
 
 //添加分类
 
 var ADDCATOGARY=DOMAIN+"m=Home&c=Index&a=addcatogary";
+
+//添加书本
+
+var ADDBOOK=DOMAIN+"m=Home&c=Index&a=add";
+var UPDATEBOOK=DOMAIN+"m=Home&c=Index&a=updatebook";
+
