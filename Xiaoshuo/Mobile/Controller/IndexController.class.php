45c1fb7d0903236a3840ef5ea588659fa6fe882f
steven9693 <704296931@qq.com>
2018-06-13 00:50:07 +0800

    修改

--- Xiaoshuo/Mobile/Controller/IndexController.class.php
+++ Xiaoshuo/Mobile/Controller/IndexController.class.php
@@ -5,21 +5,38 @@ class IndexController extends Controller {
 
 
     public function index(){
+
         $category=M('category')->where(array('isshow'=>1))->order('issort desc,category_id desc')->select();
 
         //今日推荐
         $todayrecommend=M('books')->where(array('isshow'=>1,'todayrecommend'=>1))->order('issort desc,bookid desc')->limit(3)->select();
 
         //玄幻武侠
-        $xuanhuan=M('books')->where(array('isshow'=>1,'category'=>38))->order('issort desc,bookid desc')->limit(3)->select();
+        $xh=M('books')->where(array('isshow'=>1,'category_id'=>1))->order('issort desc,bookid desc')->limit(10)->select();
+        $xuanhuan=$this->getCategory($xh,$category);
+
 
         $this->assign('todayrecommend',$todayrecommend);
 
         $this->assign('xuanhuan',$xuanhuan);
+
         $this->assign('category',$category);
+
+
 
         $this->assign('random',$this->setrandom());
         $this->display();
+    }
+
+    public function getCategory($da,$ca){
+        for($i=0;$i<count($da);$i++){
+            for($j=0;$j<count($ca);$j++){
+                if($da[$i]['category_id']==$ca[$j]['category_id']){
+                    $da[$i]['category']=$ca[$j]['name'];
+                }
+            }
+        }
+        return $da;
     }
 
 
--- xiaoshuo.sql
+++ xiaoshuo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2018-05-31 19:24:53
+-- Generation Time: 2018-06-12 18:48:01
 -- 服务器版本： 5.6.17
 -- PHP Version: 5.5.12
 
@@ -23,42 +23,57 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `loginphone` varchar(11) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `ctime` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `loginphone`, `password`, `ctime`) VALUES
+(1, '管理员', '15920581277', '39f1204e8fd1073baf9e4ef922f8f4b4', 1528131576);
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `books`
 --
 
 CREATE TABLE IF NOT EXISTS `books` (
   `bookid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `setid` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `originalurl` varchar(300) NOT NULL,
   `bookname` varchar(30) NOT NULL,
   `isshow` tinyint(4) NOT NULL,
   `issort` int(11) NOT NULL,
-  `description` int(11) NOT NULL,
+  `description` varchar(500) NOT NULL,
   `author` varchar(20) NOT NULL,
   `bojiang` varchar(20) NOT NULL,
   `status` varchar(20) NOT NULL,
   `lastupdate` int(11) NOT NULL,
   `cover` varchar(200) NOT NULL,
   `ctime` int(11) NOT NULL,
+  `todayrecommend` tinyint(4) NOT NULL,
   PRIMARY KEY (`bookid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- 转存表中的数据 `books`
 --
 
-INSERT INTO `books` (`bookid`, `category_id`, `originalurl`, `bookname`, `isshow`, `issort`, `description`, `author`, `bojiang`, `status`, `lastupdate`, `cover`, `ctime`) VALUES
-(1, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778700),
-(2, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778702),
-(3, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778705),
-(4, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778710),
-(5, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778711),
-(6, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778766),
-(7, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778853),
-(8, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778855),
-(9, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778856),
-(10, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778857),
-(11, 0, '', '', 0, 0, 0, '', '', '', 0, '', 1527778858);
+INSERT INTO `books` (`bookid`, `setid`, `category_id`, `originalurl`, `bookname`, `isshow`, `issort`, `description`, `author`, `bojiang`, `status`, `lastupdate`, `cover`, `ctime`, `todayrecommend`) VALUES
+(1, 0, 1, 'http://www.ting56.com/mp3/3178.html', '女配修仙记', 1, 0, '一边是传说中玛丽苏的女主——机遇无敌;一边是现实里的果断型女主——气运冲天，夹在其中的穿越“炮灰女”连葭葭决定保持距离是基础，努力升级是必须，扮猪吃老虎是王道!诸星元张了张嘴，多说多错，他识趣的闭上了嘴，葭葭笑过之后，也停了下来。仍然是来时的路，几人自应声墙里头出来的时候，时间已然不早了，与顾朗、秦雅告了个别，葭葭便回了自己的院子。转动了防护法阵，葭葭隐入空间之中，才一进去，就见玄灵不知从哪儿找了块巨石坐了下来，若非混沌遗世里头没有晨昏，葭葭倒是觉得，他这副悠闲的样子，配上夕阳的背景还是颇妙的。“坐。”见她进……', '漫漫步归', '猫小白', '', 1528816254, 'http://www.ting56.com/pic/images/2014-4/201441423403516054.jpg', 1528816254, 1),
+(2, 15516, 1, 'http://www.ting56.com/mp3/15516.html', '妖怪记事簿', 1, 0, '妖怪的世界，比人类进步，还是……比人类进步的多？大唐年间，唐僧远行留学天竺，可一身金贵的肉，让多少妖怪垂涎？绝世妖王孙悟空叛变妖族，妖怪界的偶像空缺有谁来填补？', '敦煌', '从容不惑', '', 1528819587, 'http://www.ting56.com/pic/images/2018-4/20184217302795437.jpg', 1528819587, 0);
 
 -- --------------------------------------------------------
 
@@ -73,27 +88,30 @@ CREATE TABLE IF NOT EXISTS `category` (
   `isshow` tinyint(4) NOT NULL,
   `ctime` int(11) NOT NULL,
   PRIMARY KEY (`category_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- 转存表中的数据 `category`
 --
 
 INSERT INTO `category` (`category_id`, `issort`, `name`, `isshow`, `ctime`) VALUES
-(1, 0, '武侠', 0, 1527701963),
-(2, 0, '仙侠', 0, 1527702045),
-(3, 0, '搞笑', 0, 1527702118),
-(4, 0, '都市', 0, 1527702368),
-(5, 0, '言情', 0, 1527702495),
-(6, 0, '', 0, 1527703632),
-(7, 0, '', 0, 1527703696),
-(8, 0, '', 0, 1527703743),
-(9, 0, '', 0, 1527703748),
-(10, 0, '', 0, 1527778955),
-(11, 0, '', 0, 1527779234),
-(12, 0, '', 0, 1527779241),
-(13, 0, '', 0, 1527779246),
-(14, 0, '', 0, 1527779313);
+(1, 0, '玄幻武侠', 1, 1528816138),
+(2, 0, '都市言情', 1, 1528816172);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `voicelist`
+--
+
+CREATE TABLE IF NOT EXISTS `voicelist` (
+  `voiceid` int(11) NOT NULL AUTO_INCREMENT,
+  `defindex` int(11) NOT NULL,
+  `voice` varchar(300) NOT NULL,
+  `setid` int(11) NOT NULL,
+  `bookid` int(11) NOT NULL,
+  PRIMARY KEY (`voiceid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
